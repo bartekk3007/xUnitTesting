ccc84a438074a52bb49e7699e5a20d6d4adeb638
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-07-13 23:53:26 +0200

    Add files via upload

---
+++ CalculatorProject/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y; 
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Divide(int x, int y)
+        {
+            if (x == 0)
+            {
+                throw new DivideByZeroException("You cannot divide by zero");
+            }
+            else
+            {
+                return (x/y);
+            }
+        }
+    }
+}
